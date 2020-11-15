5ea4e9c1bf57da28354c22fc114a57dade051d57
Dongbin Na <ndb796@naver.com>
2020-11-16 04:10:35 +0900

    Update transfer.c

--- guest/transfer.c
+++ guest/transfer.c
@@ -2,14 +2,15 @@
 
 int main(void) {
     FILE* file;
-    char buf[4096];
+    char buf[8];
 
     for (int i = 0; i < sizeof(buf); i++) {
         buf[i] = 'A';
     }
+    buf[sizeof(buf) - 1] = '\n';
 
     while (1) {
-        file = fopen("/dev/ttyGS1", "r+");
+        file = fopen("/dev/ttyGS1", "w+");
         fwrite(buf, 1, sizeof(buf), file);
         fclose(file);
     }
